9e5609355477497b14b97f97d82e4fc3b9834fd7
samlkrystof <ksaml@gapps.zcu.cz>
2023-02-21 18:00:13 +0100

    tokenize method created, small test case added

--- Tokenizers/WordPiece.py
+++ Tokenizers/WordPiece.py
@@ -1,10 +1,8 @@
+import re
 from collections import defaultdict
 from typing import List
 
-from AbstractTokenizer import AbstractTokenizer, compute_word_frequencies, compute_pair_frequencies
-import regex
-import unicodedata
-import re
+from AbstractTokenizer import AbstractTokenizer, compute_word_frequencies
 
 
 def compute_frequencies(splits: dict, word_counts: dict) -> (dict, dict):
@@ -27,6 +25,7 @@ def find_max_pair(pair_freq, letter_freq):
     pair_scores = {pair: pair_freq[pair] / (letter_freq[pair[0]] * letter_freq[pair[1]]) for pair in pair_freq}
     max_pair = max(pair_scores, key=pair_scores.get)
     return max_pair
+
 
 def update_splits(splits: dict, max_freq_pair: tuple) -> dict:
     first, second = max_freq_pair
@@ -56,8 +55,7 @@ class WordPiece(AbstractTokenizer):
         # self.regex_pattern = regex.compile("'s|'t|'re|'ve|'m|'ll|'d|\p{L}+|\p{N}+|[^\s\p{L}\p{N}]+")
 
     def train_tokenizer(self, text: str):
-        text = text.strip()
-        splitted_text = re.split(r"(\W+)", text)
+        splitted_text = self.preprocess_text(text)
 
         word_counts = compute_word_frequencies(splitted_text)
 
@@ -68,6 +66,7 @@ class WordPiece(AbstractTokenizer):
         splits = {word: [ch if i == 0 else f"##{ch}" for i, ch in enumerate(word)] for word in word_counts.keys()}
         while len(ch2i) < self.vocab_size:
             pair_freq, letter_freq = compute_frequencies(splits, word_counts)
+            print(len(pair_freq))
             max_pair = find_max_pair(pair_freq, letter_freq)
             splits = update_splits(splits, max_pair)
 
@@ -83,8 +82,43 @@ class WordPiece(AbstractTokenizer):
         self.encode_vocab = ch2i
         self.decode_vocab = {v: k for k, v in ch2i.items()}
 
+    def preprocess_text(self, text):
+        text = text.strip()
+        splitted_text = re.split(r"(\W+)", text)
+        splitted_text = [word for word in splitted_text if word != ""]
+        splitted_text = [word.strip(" ") for word in splitted_text]
+        return splitted_text
+
     def tokenize(self, text: str) -> List[str]:
-        pass
+        splitted_text = self.preprocess_text(text)
+
+
+        tokens = []
+        for word in splitted_text:
+
+            left = 0
+            right = 1
+            actual_token = "[UNK]"
+
+            print(word)
+            while left < len(word) and right <= len(word):
+                subword = word[left:right] if left == 0 else f"##{word[left:right]}"
+                if subword in self.encode_vocab:
+                    actual_token = subword
+                    right += 1
+                else:
+                    tokens.append(actual_token)
+                    if actual_token == "[UNK]":
+                        left = right
+                        right = left + 1
+                    else:
+                        left = right - 1
+                        right = left + 1
+                        actual_token = "[UNK]"
+
+            if actual_token != "[UNK]":
+                tokens.append(actual_token)
+        return tokens
 
     def encode(self, text: str) -> List[int]:
         pass
@@ -97,3 +131,16 @@ class WordPiece(AbstractTokenizer):
 
     def get_vocab(self) -> List[str]:
         pass
+
+
+if __name__ == "__main__":
+    corpus = "This is the byte pair encoder tokenizer. This is about splitting text into tokens. This class shows BPE " \
+             "tokenizer algorithm. Hopefully, you will be able to understand how they are trained to generate tokens."
+
+    tokenizer = WordPiece(100)
+    tokenizer.train_tokenizer(corpus)
+
+    print(tokenizer.tokenize("Hello!! The year which is right now is 2023. w00t :D 🤗"))
+    # encoded = tokenizer.encode("Hello!! I'm Iron man. The year which is right now is 2023. w00t :D 🤗")
+    # print(encoded)
+    # print(tokenizer.decode(encoded))
